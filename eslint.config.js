0c5e54e1c640a71587b121db95d99cabc0e65431
Crisszn <crissfx28@gmail.com>
2026-07-07 13:22:19 -0500

    simulate rules in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,13 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    rules:{
+      'no-unused-vars': 'error',     // Bloquea si hay variables sin usar
+      'no-console': 'warn',         // Mantiene los console.log como advertencia (no bloquean)
+      'react/prop-types': 'off',    // Si no usas TypeScript, esto evita falsos positivos
+      'no-undef': 'error'           // Bloquea si intentas usar algo que no existe
+      // Puedes añadir más reglas según necesites
+    },
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
